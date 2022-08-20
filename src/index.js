776f13c5607e033b9b4e69f11396596c661ab0ed
DevGibby <71801512+DevGibby@users.noreply.github.com>
2022-08-20 14:20:45 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,13 +11,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { reducers } from './redux/reducers/index';
 
-import { composeWithDevTools } from 'redux-devtools-extension';
-
-const store = createStore(reducers, composeWithDevTools(
-  applyMiddleware(thunk)
-));
-
-//const store = createStore(reducers, compose(applyMiddleware(thunk)));
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
