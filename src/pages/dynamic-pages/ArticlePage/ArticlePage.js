994cc51c1d5f95ab9542ef8eaead8768d3518b16
DevGibby <71801512+DevGibby@users.noreply.github.com>
2022-09-08 14:58:37 -0400

    meta fix

--- src/pages/dynamic-pages/ArticlePage/ArticlePage.js
+++ src/pages/dynamic-pages/ArticlePage/ArticlePage.js
@@ -42,8 +42,8 @@ export default function BlogArticle() {
                     const [ year, month, day ] = response.data.postDate.split('-');
                     setSplitDate(`${month}-${day}-${year}`);
                     document.title = `${response.data.postTitle}`;
-                    document.querySelector('meta[name="description"]').setAttribute("content", `${response.data.content.slice(0 , 100)}..`);
-                    document.querySelector('meta[name="twitter:image"]').setAttribute("content", `${response.data.thumbnail}`);
+                    document.querySelector('meta[name="og:description"]').setAttribute("content", `${response.data.content.slice(0 , 100)}..`);
+                    document.querySelector('meta[name="og:image"]').setAttribute("content", `${response.data.thumbnail}`);
                     console.log(document.head.innerHTML)
                     axios.get(`${process.env.REACT_APP_BASE_URL}/${process.env.REACT_APP_GET_CREATOR_URL}/${response.data.authorUsername}`)
                     .then(function(response){
